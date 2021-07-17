b3471da3d14fe4f0cb9fcf4c2b57e84e78977a1d
SerjSch <m89859908253@mail.com>
2021-07-17 20:25:37 +0300

    port

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 // Слушаем 3000 порт
-const PORT = 3000;
+const PORT = 3001;
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
